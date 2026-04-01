25cb88ed34f08ad9dc9649debe4d6c429709b7c0
Yogesh Pandey <yogesh1pandey0@gmail.com>
2026-04-01 14:14:33 +0530

    Updated main.c to support file input

--- src/main.c
+++ src/main.c
@@ -4,8 +4,21 @@
 #include "ast.h"
 #include "codegen.h"
 
-int main() {
-    char input[] = "print x + 5";
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: ./compiler <input_file>\n");
+        return 1;
+    }
+
+    FILE *fp = fopen(argv[1], "r");
+    if (!fp) {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    char input[100];
+    fgets(input, sizeof(input), fp);
+    fclose(fp);
 
     Token tokens[20];
     int count = tokenize(input, tokens);
